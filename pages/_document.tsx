7719b98d6de1aeba9f93bc3d9fc72c1fb2d2f048
Joshua Mathew <jm@Joshuas-iMac.local>
2023-02-03 16:17:04 +0530

    minor edits

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,67 +14,42 @@ class MyDocument extends Document {
 
     render() {
         return (
-            <Html>
-                <Head>
-                    <meta
-                        content="Epic landing page UI Design Kit for Blockchain, Crypto"
-                        name="Tesseract Blockchain Landing Page UI Design Kit"
-                    />
-                    <meta
-                        content="Tesseract Blockchain Landing Page UI Design Kit"
-                        property="og:title"
-                    />
-                    <meta
-                        content="Epic landing page UI Design Kit for Blockchain, Crypto"
-                        property="og:description"
-                    />
-                    <meta
-                        content="%PUBLIC_URL%/fb-og-image.png"
-                        property="og:image"
-                    />
-                    <meta
-                        property="og:url"
-                        content="https://ui8.net/ui8/products/tesseract-blockchain-landing-page-ui-design-kit"
-                    />
-                    <meta
-                        property="og:site_name"
-                        content="Tesseract Blockchain Landing Page UI Design Kit"
-                    />
-                    <meta
-                        content="Tesseract Blockchain Landing Page UI Design Kit"
-                        property="twitter:title"
-                    />
-                    <meta
-                        content="Epic landing page UI Design Kit for Blockchain, Crypto"
-                        property="twitter:description"
-                    />
-                    <meta
-                        content="%PUBLIC_URL%/twitter-card.png"
-                        property="twitter:image"
-                    />
-                    <meta property="og:type" content="Article" />
-                    <meta content="summary" name="twitter:card" />
-                    <meta name="twitter:site" content="@ui8" />
-                    <meta name="twitter:creator" content="@ui8" />
-                    <meta property="fb:admins" content="132951670226590" />
-                    <meta
-                        name="viewport"
-                        content="width=device-width, initial-scale=1"
-                    />
-                    <meta name="theme-color" content="#000000" />
-                    <meta name="msapplication-TileColor" content="#da532c" />
-                    <meta name="theme-color" content="#ffffff" />
-                    <link
-                        href="https://fonts.googleapis.com/css2?family=Inter:wght@700;900&family=Karla:wght@400;500;800&display=swap"
-                        rel="stylesheet"
-                    />
-                </Head>
-                <body className="app">
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
+					<Html>
+						<Head>
+							<meta
+								content='Tesseract | Decentralized Infrastructure for Games'
+								name='Tesseract XYZ'
+							/>
+							<meta
+								content='Tesseract | Decentralized Infrastructure for Games'
+								property='og:title'
+							/>
+							<meta
+								content='Tesseract | Decentralized Infrastructure for Games'
+								property='og:description'
+							/>
+							<meta
+								content='%PUBLIC_URL%/fb-og-image.png'
+								property='og:image'
+							/>
+							<meta
+								name='viewport'
+								content='width=device-width, initial-scale=1'
+							/>
+							<meta name='theme-color' content='#000000' />
+							<meta name='msapplication-TileColor' content='#da532c' />
+							<meta name='theme-color' content='#ffffff' />
+							<link
+								href='https://fonts.googleapis.com/css2?family=Inter:wght@700;900&family=Karla:wght@400;500;800&display=swap'
+								rel='stylesheet'
+							/>
+						</Head>
+						<body className='app'>
+							<Main />
+							<NextScript />
+						</body>
+					</Html>
+				)
     }
 }
 
