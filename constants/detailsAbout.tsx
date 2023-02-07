a41bdc51898b94def96b91a40f5e37bb97431307
Joshua Mathew <jm@Joshuas-iMac.local>
2023-02-07 10:38:55 +0530

    edits

--- constants/detailsAbout.tsx
+++ constants/detailsAbout.tsx
@@ -1,48 +1,45 @@
 export const details = [
-    {
-        image: {
-            src: "/images/about-pic-1.png",
-            width: 712,
-            height: 712,
-            alt: "Figure",
-        },
-        content: (
-            <>
-                <h2>New Framework for Games</h2>
-                <p>
-                    Tesseract delivers tamper-proof inputs, outputs, and computations
-                    to support advanced player interactions on any EVM blockchain.
-                </p>
-                <ul>
-                    <li>Open source and composable EVM contracts</li>
-                    <li>Inherently more secure than all centralized providers</li>
-                    <li>More transparent and powerful with 0.001% downtime</li>
-                </ul>
-            </>
-        ),
-    },
-    {
-        image: {
-            src: "/images/about-pic-2.png",
-            width: 712,
-            height: 682,
-            alt: "Figure",
-        },
-        content: (
-            <>
-                <h2>Why build on Tesseract?</h2>
-                <p>
-                    Tesseract is committed to fostering the growth of Web3
-                    applications by providing the infrastructure and security
-                    needed for Web3.
-                </p>
-                <p>
-                    Tesseract believes in Web3 for all. Tesseract is a decentralised
-                    blockchain scaling platform that enables developers to build
-                    scalable user-friendly dApps with low transaction fees
-                    without ever sacrificing on security.
-                </p>
-            </>
-        ),
-    },
-];
+	{
+		image: {
+			src: '/images/about-pic-1.png',
+			width: 712,
+			height: 712,
+			alt: 'Figure',
+		},
+		content: (
+			<>
+				<h2>New Framework for Games</h2>
+				<p>
+					Tesseract delivers tamper-proof inputs, outputs, and computations to
+					support advanced player interactions on any EVM blockchain.
+				</p>
+				<ul>
+					<li>Open source and composable EVM contracts</li>
+					<li>Inherently more secure than all centralized providers</li>
+					<li>More transparent and powerful with 0.001% downtime</li>
+				</ul>
+			</>
+		),
+	},
+	{
+		image: {
+			src: '/images/about-pic-2.png',
+			width: 712,
+			height: 682,
+			alt: 'Figure',
+		},
+		content: (
+			<>
+				<h2>Why build on Tesseract?</h2>
+				<p>
+					Spend less time writing smart contracts, more time building fun games
+				</p>
+				<p>
+					Tesseract is the first decentralized protocol designed from the ground
+					up to enable creators and developers to build experiences that cater
+					to the next billion users in web3.
+				</p>
+			</>
+		),
+	},
+]
